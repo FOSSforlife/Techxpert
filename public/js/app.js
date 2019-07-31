4fd26bd70603bdf8e8ffc9870ea5d274185ec698
Canyon Schwartz <canyon.schwartz@gmail.com>
2019-07-30 17:17:56 -0700

    Added temp flows for phone and desktop device

--- public/js/app.js
+++ public/js/app.js
@@ -1,50 +1,79 @@
 let currentQuestion = 0;
-let budgetButton = document.getElementById('budget-button');
+let deviceChoice = 'null';
+let questionTitle = document.getElementById('question-title');
+let answerA = document.getElementById('answer-a');
+let answerB = document.getElementById('answer-b');
+let answerC = document.getElementById('answer-c');
+let answerD = document.getElementById('answer-d');
+let answerE = document.getElementById('answer-e');
 
-window.onload = function () {
-  document.getElementById('question-page').style.display = 'hide';
-  document.getElementById('budget-slider').style.display = 'hide';
-}
+document.getElementById('laptop-device').addEventListener('click', function() {
+  laptopQuestions();
+  deviceChoice = 'laptop'
+})
 
-// Obtains the budget from sliders after clicking next
-budgetButton.addEventListener('click', function () {
-  let min = document.getElementById('slider-min').value;
-  let max = document.getElementById('slider-max').value;
-  console.log(min, max);
-  document.getElementById('question-page').style.display = 'inline';
-  document.getElementById('budget-slider').style.display = 'none';
-});
+document.getElementById('desktop-device').addEventListener('click', function() {
+  desktopQuestions();
+  deviceChoice = 'desktop';
+})
+
+document.getElementById('phone-device').addEventListener('click', function() {
+  phoneQuestions();
+  deviceChoice = 'phone';
+})
+
+document.getElementById('answer-section').addEventListener('click', function() {
+  currentQuestion = currentQuestion + 1;
+
+  if (deviceChoice == 'laptop') {
+    laptopQuestions();
+  } else if (deviceChoice == 'desktop') {
+    desktopQuestions();
+  } else if (deviceChoice == 'phone') {
+    phoneQuestions();
+  }
+})
 
 // Function to ask laptop questions
 function laptopQuestions() {
   document.getElementById('home-page').style.display = 'none';
-  document.getElementById('budget-slider').style.display = 'inline';
-  document.getElementById('question-page').style.display = 'none';
+  document.getElementById('question-page').style.display = 'inline';
 
-  const answerChoices = ['answer-a', 'answer-b', 'answer-c', 'answer-d', 'answer-e'];
+  const answerChoices =
+      ['answer-a', 'answer-b', 'answer-c', 'answer-d', 'answer-e'];
 
-  const questions = [{
-      title: 'What operating system do you prefer?',
-      answers: ['Mac', 'Windows', 'Either'],
-      template: 'template1'
+  const questions = [
+    {
+      title: 'What is your budget?',
+      answers: [
+        '$400-$999', '$1000-$1399', '$1400-$1699', '$1700-$1999', '$2000 and Up'
+      ]
     },
     {
-      title: 'What do you plan on using this laptop for?',
-      answers: ['School', 'Work', 'Gaming', 'Personal'],
-      template: 'template1'
+      title:
+          'Which operating system do you prefer? If unsure, choose Microsoft Windows',
+      answers: ['Microsoft Windows', 'Apple MacOS']
     },
     {
-      title: 'Does the laptop need to be portable?',
-      answers: ['Very portable', 'Fairly Portable', 'Not Important'],
-      template: 'template1'
+      title: 'What will be the primary use of this laptop?',
+      answers: [
+        'Web Browsing/Office Work', 'Gaming',
+        'Heavy Computing - Video Editing, Programming', 'Travel'
+      ]
     },
+    {
+      title: 'How much do you value portability and battery life?',
+      answers: [
+        'Very much - will be traveling with laptop/moving it around a lot',
+        'Somewhat', 'Not at all'
+      ]
+    }
   ];
 
   if (currentQuestion >= questions.length) {
-    document.getElementById('question').innerHTML = 'DONE';
+    questionTitle.innerHTML = 'DONE';
   } else {
-    document.getElementById('question').innerHTML =
-      questions[currentQuestion].title;
+    questionTitle.innerHTML = questions[currentQuestion].title;
     let currentAnswers = questions[currentQuestion].answers;
     for (let i = 0; i < answerChoices.length; i++) {
       if (i < currentAnswers.length) {
@@ -54,9 +83,108 @@ function laptopQuestions() {
       }
     }
   }
-
-  currentQuestion = currentQuestion + 1;
 }
+
+function desktopQuestions() {
+  document.getElementById('home-page').style.display = 'none';
+  document.getElementById('question-page').style.display = 'inline';
+
+  const answerChoices =
+      ['answer-a', 'answer-b', 'answer-c', 'answer-d', 'answer-e'];
+
+  const questions = [
+    {
+      title: 'DESKTOP-TEST',
+      answers: [
+        '$400-$999', '$1000-$1399', '$1400-$1699', '$1700-$1999', '$2000 and Up'
+      ],
+    },
+    {
+      title:
+          'Which operating system do you prefer? If unsure, choose Microsoft Windows',
+      answers: ['Microsoft Windows', 'Apple MacOS'],
+    },
+    {
+      title: 'What will be the primary use of this laptop?',
+      answers: [
+        'Web Browsing/Office Work', 'Gaming',
+        'Heavy Computing - Video Editing, Programming', 'Travel'
+      ],
+    },
+    {
+      title: 'How much do you value portability and battery life?',
+      answers: [
+        'Very much - will be traveling with laptop/moving it around a lot',
+        'Somewhat', 'Not at all'
+      ],
+    }
+  ];
+
+  if (currentQuestion >= questions.length) {
+    questionTitle.innerHTML = 'DONE';
+  } else {
+    questionTitle.innerHTML = questions[currentQuestion].title;
+    let currentAnswers = questions[currentQuestion].answers;
+    for (let i = 0; i < answerChoices.length; i++) {
+      if (i < currentAnswers.length) {
+        document.getElementById(answerChoices[i]).innerHTML = currentAnswers[i];
+      } else {
+        document.getElementById(answerChoices[i]).innerHTML = 'NA';
+      }
+    }
+  }
+}
+
+function phoneQuestions() {
+  document.getElementById('home-page').style.display = 'none';
+  document.getElementById('question-page').style.display = 'inline';
+
+  const answerChoices =
+      ['answer-a', 'answer-b', 'answer-c', 'answer-d', 'answer-e'];
+
+  const questions = [
+    {
+      title: 'PHONE TEST',
+      answers: [
+        '$400-$999', '$1000-$1399', '$1400-$1699', '$1700-$1999', '$2000 and Up'
+      ],
+    },
+    {
+      title:
+          'Which operating system do you prefer? If unsure, choose Microsoft Windows',
+      answers: ['Microsoft Windows', 'Apple MacOS'],
+    },
+    {
+      title: 'What will be the primary use of this laptop?',
+      answers: [
+        'Web Browsing/Office Work', 'Gaming',
+        'Heavy Computing - Video Editing, Programming', 'Travel'
+      ],
+    },
+    {
+      title: 'How much do you value portability and battery life?',
+      answers: [
+        'Very much - will be traveling with laptop/moving it around a lot',
+        'Somewhat', 'Not at all'
+      ],
+    }
+  ];
+
+  if (currentQuestion >= questions.length) {
+    questionTitle.innerHTML = 'DONE';
+  } else {
+    questionTitle.innerHTML = questions[currentQuestion].title;
+    let currentAnswers = questions[currentQuestion].answers;
+    for (let i = 0; i < answerChoices.length; i++) {
+      if (i < currentAnswers.length) {
+        document.getElementById(answerChoices[i]).innerHTML = currentAnswers[i];
+      } else {
+        document.getElementById(answerChoices[i]).innerHTML = 'NA';
+      }
+    }
+  }
+}
+
 
 function hide(letter) {
   document.getElementById('card-answer-' + letter).style.display = 'none';
@@ -83,29 +211,29 @@ function toggleChecks() {
   })
 }
 
-document.querySelector("#answer-a").addEventListener("click", addLaptop);
-document.querySelector("#answer-b").addEventListener("click", addDesktop);
-document.querySelector("#answer-c").addEventListener("click", addPhone);
-document.querySelector("#answer-d").addEventListener("click", addTablet);
+document.querySelector('#answer-a').addEventListener('click', addLaptop);
+document.querySelector('#answer-b').addEventListener('click', addDesktop);
+document.querySelector('#answer-c').addEventListener('click', addPhone);
+document.querySelector('#answer-d').addEventListener('click', addTablet);
 
 var choice = {};
 
 function addLaptop() {
-  choice["device"] = "laptop";
+  choice['device'] = 'laptop';
   console.log(choice);
 }
 
 function addDekstop() {
-  choice("device") = "Desktop";
+  choice('device') = 'Desktop';
   console.log(choice);
 }
 
 function addPhone() {
-  choice("device") = "Phone";
+  choice('device') = 'Phone';
   console.log(choice);
 }
 
 function addTablet() {
-  choice("device") = "Tablet";
+  choice('device') = 'Tablet';
   console.log(choice);
 }
